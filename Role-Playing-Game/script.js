743a7cd9703ca18164aa1ac7a60d0a9bd63eeb9a
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-09 22:01:49 +0500

    used a function

--- Role-Playing-Game/script.js
+++ Role-Playing-Game/script.js
@@ -16,3 +16,13 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
+}
