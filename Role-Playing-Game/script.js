91ee33b525063266425f55206d08e77ba34d801c
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-10 20:06:36 +0500

    locations array for repeated functions

--- Role-Playing-Game/script.js
+++ Role-Playing-Game/script.js
@@ -17,6 +17,21 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
+  }
+];
+
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
