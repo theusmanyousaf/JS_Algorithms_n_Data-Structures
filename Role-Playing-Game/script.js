6e88e8f7a5ccb52d72c606eccac8e2b850fc2971
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-11 07:37:42 +0500

    buying health with gold

--- Role-Playing-Game/script.js
+++ Role-Playing-Game/script.js
@@ -70,7 +70,14 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health."
+  }
 }
 
 function buyWeapon() {
