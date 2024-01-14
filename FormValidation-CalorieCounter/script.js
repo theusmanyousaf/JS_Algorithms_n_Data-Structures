9a290a676f4984185aea58bbb376cc627ffaeb4b
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-14 22:06:31 +0500

    entry addition

--- FormValidation-CalorieCounter/script.js
+++ FormValidation-CalorieCounter/script.js
@@ -11,3 +11,19 @@ function cleanInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, '');
 }
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input
+      type="number"
+      min="0"
+      id="${entryDropdown.value}-${entryNumber}-calories"
+      placeholder="Calories"
+    />`;
+    targetInputContainer.innerHTML += HTMLString;
+  }
