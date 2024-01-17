b084c04dde7ff0dc9eb65b025fda0b76bd043440
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-17 21:56:42 +0500

    getting calories from user input

--- FormValidation-CalorieCounter/script.js
+++ FormValidation-CalorieCounter/script.js
@@ -39,5 +39,13 @@ function getCaloriesFromInputs(list) {
   for (let i = 0; i < list.length; i++) {
     const currVal = cleanInputString(list[i].value);
     const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
   }
+  return calories;
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 <h1 align="center">JavaScript Algorithms and Data Structures</h1>
 <h3>1.Learn Basic JavaScript by Building a Role Playing Game</h3>
+<h3>2.Learn Form Validation by Building a Calorie counter</h3>
