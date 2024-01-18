261dc041676083480858e2c49b1a17dcfe1b93fb
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-18 23:55:54 +0500

    calories calculation

--- FormValidation-CalorieCounter/script.js
+++ FormValidation-CalorieCounter/script.js
@@ -33,6 +33,18 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+
+  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0;
 
