300a2cf5db26b08af76b31fca07b44a9f8757efb
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-19 21:46:12 +0500

    addEntry&calorieCounter buttons EventListeners

--- FormValidation-CalorieCounter/script.js
+++ FormValidation-CalorieCounter/script.js
@@ -85,3 +85,6 @@ function getCaloriesFromInputs(list) {
   }
   return calories;
 }
+
+addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
