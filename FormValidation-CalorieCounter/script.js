a0a1c462e6908bc74570785caab0063accdbe6df
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-19 21:40:46 +0500

    Revert "calories input in calculateCalories function"
    This reverts commit afcbfb9f16f9d0d3dfb17ef0dd75629222503757.

--- FormValidation-CalorieCounter/script.js
+++ FormValidation-CalorieCounter/script.js
@@ -34,7 +34,7 @@ function addEntry() {
 }
 
 function calculateCalories(e) {
-  e.preventDefault();  // to prevent clearing of data after form submission
+  e.preventDefault();
   isError = false;
 
   const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
@@ -42,17 +42,6 @@ function calculateCalories(e) {
   const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
   const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
   const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
-
-  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
-  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
-  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
-  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
-  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
-  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
-
-  if (isError) {
-    return;
-  }
 
 }
 
