279e68d1c600e89ae6769020e8ce0088312e191f
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-20 21:16:24 +0500

    Form Clearing option

--- FormValidation-CalorieCounter/script.js
+++ FormValidation-CalorieCounter/script.js
@@ -86,5 +86,18 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
+function clearForm() {
+  const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+
+  for (let i = 0; i < inputContainers.length; i++) {
+    inputContainers[i].innerHTML = '';
+  }
+
+  budgetNumberInput.value = '';
+  output.innerText = '';
+  output.classList.add('hide');
+}
+
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click", clearForm);
