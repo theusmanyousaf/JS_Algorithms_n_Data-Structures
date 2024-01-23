438969923d12d11c26d2e9f2f2822159c2e0d5d1
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-24 00:10:23 +0500

    reset functionality

--- StringArrayMethods-MusicPlayer/script.js
+++ StringArrayMethods-MusicPlayer/script.js
@@ -161,7 +161,24 @@ const deleteSong = (id) => {
     highlightCurrentSong();
     setPlayButtonAccessibleText();
 
+    if (userData.songs.length === 0) {
+        const resetButton = document.createElement("button");
+        const resetText = document.createTextNode("Reset Playlist");
 
+        resetButton.id = "reset";
+        resetButton.ariaLabel = "Reset playlist";
+        resetButton.appendChild(resetText);
+        playlistSongs.appendChild(resetButton);
+
+        resetButton.addEventListener("click", () => {
+            userData.songs = [...allSongs];
+
+            renderSongs(userData?.songs);
+            setPlayButtonAccessibleText();
+            resetButton.remove();
+        });
+
+    }
 
 };
 
