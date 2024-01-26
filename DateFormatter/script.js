94ccc645a283b580a71fda6aae452c609c16349d
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-26 23:40:41 +0500

    Date object

---
+++ DateFormatter/script.js
@@ -0,0 +1,15 @@
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date(); 
+
+/* 
+the Date() constructor with the new operator to create a new Date object that returns
+a string with the current date and time:
+
+const currentDate = new Date();
+console.log(currentDate);
+
+Output:
+Mon Aug 23 2021 15:31:00 GMT-0400 (Eastern Daylight Time)
+*/
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <h1 align="center">JavaScript Algorithms and Data Structures</h1>
 <h3>1.Learn Basic JavaScript by Building a Role Playing Game</h3>
 <h3>2.Learn Form Validation by Building a Calorie counter</h3>
-<h3>2.Learn Basic Stirng and Array methods by Building a Music Player</h3>
+<h3>3.Learn Basic Stirng and Array methods by Building a Music Player</h3>
+<h3>4.Learn Date Object by Building a Date Formatter</h3>
