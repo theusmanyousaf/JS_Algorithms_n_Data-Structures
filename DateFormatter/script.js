03d7269310faaa5d096080e9c8203b7e37d68c00
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-27 23:57:57 +0500

    Current Date UI

--- DateFormatter/script.js
+++ DateFormatter/script.js
@@ -1,7 +1,7 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
-const date = new Date(); 
+const date = new Date();
 
 /* 
 the Date() constructor with the new operator to create a new Date object that returns
@@ -13,3 +13,12 @@ console.log(currentDate);
 Output:
 Mon Aug 23 2021 15:31:00 GMT-0400 (Eastern Daylight Time)
 */
+
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
