c2fb3f26268fb9189887614f10b441d8d0510d83
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-01 09:27:57 +0500

    cards filtering

--- FootballTeamCards/script.js
+++ FootballTeamCards/script.js
@@ -200,3 +200,34 @@ const setPlayerCards = (arr = players) => {
         )
         .join(""); // join method to avoid showing of comma separation which occurs as a result of map method
 };
+
+playersDropdownList.addEventListener("change", (e) => { // 'e' represents an object which contains the information for that event.
+    playerCards.innerHTML = "";
+
+    switch (e.target.value) {
+        case "nickname":
+            setPlayerCards(players.filter((player) => player.nickname !== null));
+            break;
+        case "forward":
+            setPlayerCards(players.filter((player) => player.position === "forward"));
+            break;
+        case "midfielder":
+            setPlayerCards(
+                players.filter((player) => player.position === "midfielder")
+            );
+            break;
+        case "defender":
+            setPlayerCards(
+                players.filter((player) => player.position === "defender")
+            );
+            break;
+        case "goalkeeper":
+            setPlayerCards(
+                players.filter((player) => player.position === "goalkeeper")
+            );
+            break;
+
+        default:
+            setPlayerCards();
+    }
+});
