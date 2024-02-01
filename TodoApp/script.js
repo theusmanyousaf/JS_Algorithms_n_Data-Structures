2e9a3e4761536960c3ef4b959b370f40bb5c8427
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-01 12:12:45 +0500

    taskFormBtn showModal method

--- TodoApp/script.js
+++ TodoApp/script.js
@@ -1,3 +1,7 @@
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
@@ -12,5 +16,9 @@ const taskData = []
 let currentTask = {} // This variable will be used to track the state when editing and discarding tasks.
 
 openTaskFormBtn.addEventListener("click", () =>
-  taskForm.classList.toggle("hidden") // The toggle method will add the class if it is not present on the element, and remove the class if it is present on the element.
+    taskForm.classList.toggle("hidden") // The toggle method will add the class if it is not present on the element, and remove the class if it is present on the element.
 );
+
+closeTaskFormBtn.addEventListener("click", () => {
+    confirmCloseDialog.showModal(); // The HTML dialog element has a showModal() method that can be used to display a modal dialog box on a web page.
+});
