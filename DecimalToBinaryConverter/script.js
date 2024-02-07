28ab07a7e4b8686b40b51ef7af02a96a842b278a
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-07 23:07:18 +0500

    inputs division

--- DecimalToBinaryConverter/script.js
+++ DecimalToBinaryConverter/script.js
@@ -2,13 +2,32 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+    const inputs = [];
+    const quotients = [];
+    const remainders = [];
+
+    while (input > 0) {
+        const quotient = Math.floor(input / 2);
+        const remainder = input % 2;
+
+        inputs.push(input);
+        quotients.push(quotient);
+        remainders.push(remainder);
+        input = quotient;
+    }
+
+    console.log("Inputs: ", inputs);
+}
+
 const checkUserInput = () => {
     if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
         alert("Please provide a decimal number");
         return;
     }
 
-    console.log(numberInput.value);
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = ""; // to clear input after each value check
 };
 
 convertBtn.addEventListener("click", checkUserInput);
