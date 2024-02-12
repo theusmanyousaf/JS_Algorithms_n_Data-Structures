01e6302ba287572ee72a838703fff5b784c4135b
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-12 18:36:20 +0500

    Animation on recursive function with value 5

--- DecimalToBinaryConverter/script.js
+++ DecimalToBinaryConverter/script.js
@@ -2,12 +2,56 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const animationData = [
+    {
+        inputVal: 5,
+        marginTop: 300,
+        addElDelay: 1000
+    },
+    {
+        inputVal: 2,
+        marginTop: -200,
+        addElDelay: 1500
+    },
+    {
+        inputVal: 1,
+        marginTop: -200,
+        addElDelay: 2000
+    }
+];
+
 const decimalToBinary = (input) => {
     if (input === 0 || input === 1) {
         return String(input); // returns input value as a string
     } else {
         return decimalToBinary(Math.floor(input / 2)) + (input % 2);
     }
+};
+
+const showAnimation = () => {
+    result.innerText = "Call Stack Animation";
+
+    animationData.forEach((obj) => {
+        setTimeout(() => {
+            animationContainer.innerHTML += `
+          <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
+            decimalToBinary(${obj.inputVal})
+          </p>
+        `;
+        }, obj.addElDelay);
+
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).textContent = obj.msg;
+        }, obj.showMsgDelay);
+
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).remove();
+        }, obj.removeElDelay);
+    });
+
+    setTimeout(() => {
+        result.textContent = decimalToBinary(5);
+    }, 20000);
 };
 
 const checkUserInput = () => {
