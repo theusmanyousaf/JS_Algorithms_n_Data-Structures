97cfb15a5c3ac563f73cd97d198dc5c39fe0ae52
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-17 23:28:32 +0500

    sortInput function

---
+++ Number-Sorter/script.js
@@ -0,0 +1,9 @@
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+    event.preventDefault(); // to prevent default submission of form
+
+
+}
+
+sortButton.addEventListener("click", sortInputArray);
