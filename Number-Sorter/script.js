a6246d9950b9557f9c14c2352f99fe4b87566ad9
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-18 19:54:37 +0500

    update ui

--- Number-Sorter/script.js
+++ Number-Sorter/script.js
@@ -2,8 +2,17 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
     event.preventDefault(); // to prevent default submission of form
+    // Since we have an array, we can use the map method to convert each value to a number.
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+    updateUI(inputValues)
+}
 
 
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+        outputValueNode.innerText = num;
+    })
 }
 
 sortButton.addEventListener("click", sortInputArray);
