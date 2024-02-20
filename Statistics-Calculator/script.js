b19d36fa773672405cd6d459bdc058612d6c5e77
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-20 09:27:49 +0500

    cal mean

--- Statistics-Calculator/script.js
+++ Statistics-Calculator/script.js
@@ -1,3 +1,10 @@
+const getMean = (array) => {
+    // this method iterates over the array and return one condenced value of the array in this case return sum of all values of the array
+    const sum = array.reduce((acc, el) => acc + el, 0); // this initialize the accumulator with 0 value
+    const mean = sum / array.length;
+    return mean;
+}
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value; // selects input element with id 'numbers' and returns its value entered by user
     const array = value.split(/,\s*/g); // splits values entered in the form of an array separated by commas regrdless of spaces between them
