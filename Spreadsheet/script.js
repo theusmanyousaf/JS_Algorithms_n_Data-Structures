0f427059d83e5fbcc7fd08ecb653aa492c8da8d3
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-23 00:11:47 +0500

    spreadsheet functions

--- Spreadsheet/script.js
+++ Spreadsheet/script.js
@@ -6,7 +6,15 @@ const median = nums => {
     const sorted = nums.slice().sort((a, b) => a - b);
     const length = sorted.length;
     const middle = length / 2 - 1;
-    return isEven(length) ? average([sorted[middle], sorted[middle + 1]]) : sorted[Math.ceil(middle)];
+    return isEven(length)
+        ? average([sorted[middle], sorted[middle + 1]])
+        : sorted[Math.ceil(middle)];
+}
+
+const spreadsheetFunctions = { // to keep track of built-in functions
+    sum,
+    average,
+    median
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
