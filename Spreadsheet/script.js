2b914cb63feeeecb4dc297503691765304026542
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-23 22:00:44 +0500

    cells calculation formula

--- Spreadsheet/script.js
+++ Spreadsheet/script.js
@@ -20,6 +20,18 @@ const spreadsheetFunctions = { // to keep track of built-in functions
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
+
+const evalFormula = (x, cells) => {
+    const idToText = id => cells.find(cell => cell.id === id).value;
+    const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+    const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+    const elemValue = num => character => idToText(character + num);
+    const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
+    const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
+    const cellRegex = /[A-J][1-9][0-9]?/gi;
+    const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
+}
+
 window.onload = () => {
     const container = document.getElementById("container");
     const createLabel = (name) => {
