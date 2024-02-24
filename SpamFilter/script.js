eb2cc6e8987cf05fd32f5d7615dd491d6994b504
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-24 12:00:06 +0500

    spam msg and eventlistener

--- SpamFilter/script.js
+++ SpamFilter/script.js
@@ -1,3 +1,12 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
+
+const isSpam = (msg) => false;
+
+checkMessageButton.addEventListener("click", () => {
+    if (messageInput.value === "") {
+        alert("Please enter a message.");
+        return;
+    }
+})
