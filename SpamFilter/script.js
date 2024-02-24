681f6da9affaf7d2d58b1a79e5d6aaa48cb98586
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-24 12:56:07 +0500

    button press eventlistener

--- SpamFilter/script.js
+++ SpamFilter/script.js
@@ -23,3 +23,18 @@ checkMessageButton.addEventListener("click", () => {
         : "This message does not seem to contain any spam.";
     messageInput.value = "";
 });
+
+messageInput.addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        if (messageInput.value === "") {
+            alert("Please enter a message.");
+            return;
+        }
+
+        result.textContent = isSpam(messageInput.value)
+            ? "Oh no! This looks like a spam message."
+            : "This message does not seem to contain any spam.";
+        messageInput.value = "";
+    }
+});
