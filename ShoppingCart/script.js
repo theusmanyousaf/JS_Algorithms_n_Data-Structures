25059596bd168648c61566d42aaf9fd3519dcfba
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-28 23:20:09 +0500

    shopping cart ui

--- ShoppingCart/script.js
+++ ShoppingCart/script.js
@@ -119,5 +119,19 @@ class ShoppingCart {
         this.items.forEach((dessert) => {
             totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
         })
+
+        const currentProductCount = totalCountPerProduct[product.id];
+        const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+
+        currentProductCount > 1
+            ? currentProductCountSpan.textContent = `${currentProductCount}x`
+            : productsContainer.innerHTML += `
+        <div id=dessert${id} class="product">
+          <p>
+            <span class="product-count" id=product-count-for-id${id}></span>${name}
+          </p>
+          <p>${price}</p>
+        </div>
+        `;
     }
 };
