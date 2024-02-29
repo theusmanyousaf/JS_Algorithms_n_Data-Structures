f5b33f82f6b842fc5ce4ccfaba10685566e0e114
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-29 22:12:53 +0500

    total amount incl taxes

--- ShoppingCart/script.js
+++ ShoppingCart/script.js
@@ -138,6 +138,20 @@ class ShoppingCart {
     getCounts() {
         return this.items.length;
     }
+
+    calculateTaxes(amount) {
+        return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+    }
+
+    calculateTotal() {
+        const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+        const tax = this.calculateTaxes(subTotal);
+        this.total = subTotal + tax;
+        cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+        cartTaxes.textContent = `$${tax.toFixed(2)}`;
+        cartTotal.textContent = `$${this.total.toFixed(2)}`;
+        return this.total;
+    }
 };
 
 const cart = new ShoppingCart();
@@ -148,6 +162,7 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
         btn.addEventListener("click", (event) => {
             cart.addItem(Number(event.target.id), products);
             totalNumberOfItems.textContent = cart.getCounts();
+            cart.calculateTotal();
         })
     }
 );
