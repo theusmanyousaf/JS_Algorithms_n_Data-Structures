1d01c55e7cb7387e8314b0c3dc1cb2be8168d8e6
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-02-29 22:24:25 +0500

    cart clearing method

--- ShoppingCart/script.js
+++ ShoppingCart/script.js
@@ -139,6 +139,27 @@ class ShoppingCart {
         return this.items.length;
     }
 
+    clearCart() {
+        if (!this.items.length) {
+            alert("Your shopping cart is already empty");
+            return;
+        }
+
+        const isCartCleared = confirm(
+            "Are you sure you want to clear all items from your shopping cart?"
+        );
+
+        if (isCartCleared) {
+            this.items = [];
+            this.total = 0;
+            productsContainer.innerHTML = "";
+            totalNumberOfItems.textContent = 0;
+            cartSubTotal.textContent = 0;
+            cartTaxes.textContent = 0;
+            cartTotal.textContent = 0;
+        }
+    }
+
     calculateTaxes(amount) {
         return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
     }
@@ -172,3 +193,5 @@ cartBtn.addEventListener("click", () => {
     showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
     cartContainer.style.display = isCartShowing ? "block" : "none";
 });
+
+clearCartBtn.addEventListener("click", cart.clearCart.bind(cart));
