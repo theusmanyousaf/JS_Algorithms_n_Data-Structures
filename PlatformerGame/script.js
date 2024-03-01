6839c636ffed45a084ffbea59f3e21f0b72cc5f6
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-01 10:41:02 +0500

    Player & player

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -13,3 +13,48 @@ let isCheckpointCollisionDetectionActive = true;
 const proportionalSize = (size) => {
     return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 }
+
+class Player {
+    constructor() {
+        this.position = {
+            x: proportionalSize(10),
+            y: proportionalSize(400),
+        };
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+        this.width = proportionalSize(40);
+        this.height = proportionalSize(40);
+    }
+    draw() {
+        ctx.fillStyle = "#99c9ff"; // player color
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height); // player body to be filled with color
+    }
+
+
+    update() {
+        this.draw(); // to make sure player is always drawing on the screen;
+        this.position.x += this.velocity.x; // when player moves to adjust its position according to its velocity
+        this.position.y += this.velocity.y; // when player jumps
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) { // to make sure player do not jump past canvas height
+            if (this.position.y < 0) {
+                this.position.y = 0;
+                this.velocity.y = gravity;
+            }
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
+
+        if (this.position.x < this.width) { // to make sure player do not move to far off to the left
+            this.position.x = this.width;
+        }
+        if (this.position.x >= canvas.width - 2 * this.width) { // so the player does not go off screen to the right
+            this.position.x = canvas.width - 2 * this.width;
+        }
+    }
+}
+
+const player = new Player();
