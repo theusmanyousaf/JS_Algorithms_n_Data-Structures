03936ea79d86457a270f3934b09c731557e05894
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-01 10:45:45 +0500

    Start Game

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -58,3 +58,11 @@ class Player {
 }
 
 const player = new Player();
+
+const startGame = () => {
+    canvas.style.display = "block";
+    startScreen.style.display = "none";
+    player.draw();
+}
+
+startBtn.addEventListener("click", startGame);
