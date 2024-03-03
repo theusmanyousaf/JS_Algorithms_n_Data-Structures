74c1c766c280cbf27f698582deda1e1246676825
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-03 22:50:18 +0500

    Platform detection rules

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -133,6 +133,19 @@ const animate = () => {
             player.velocity.y = 0;
             return;
         }
+
+        const platformDetectionRules = [
+            player.position.x >= platform.position.x - player.width / 2,
+            player.position.x <=
+            platform.position.x + platform.width - player.width / 3,
+            player.position.y + player.height >= platform.position.y,
+            player.position.y <= platform.position.y + platform.height,
+        ];
+
+        if (platformDetectionRules.every(rule => rule)) {
+            player.position.y = platform.position.y + player.height;
+            player.velocity.y = gravity;
+        };
     });
 
 }
