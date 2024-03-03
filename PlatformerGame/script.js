3417ef4f20f88b5efb7928cceeee0b314f2e2151
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-03 23:40:05 +0500

    Check reached logic & message

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -194,6 +194,30 @@ const animate = () => {
         };
     });
 
+    checkpoints.forEach((checkpoint, index, checkpoints) => {
+        const checkpointDetectionRules = [
+            player.position.x >= checkpoint.position.x,
+            player.position.y >= checkpoint.position.y,
+            player.position.y + player.height <=
+            checkpoint.position.y + checkpoint.height,
+            isCheckpointCollisionDetectionActive,
+            player.position.x - player.width <=
+            checkpoint.position.x - checkpoint.width + player.width * 0.9,
+            index === 0 || checkpoints[index - 1].claimed === true,
+        ];
+
+        if (checkpointDetectionRules.every((rule) => rule)) {
+            checkpoint.claim();
+
+            if (index === checkpoints.length - 1) { // if player has reached last checkpoint
+                isCheckpointCollisionDetectionActive = false;
+                showCheckpointScreen("You reached the final checkpoint!");
+                movePlayer("ArrowRight", 0, false);
+            } else if (player.position.x >= checkpoint.position.x && player.position.x <= checkpoint.position.x + 40) {
+                showCheckpointScreen("You reached a checkpoint!")
+            }
+        };
+    });
 }
 
 const keys = {
@@ -239,6 +263,14 @@ const startGame = () => {
     startScreen.style.display = "none";
     animate();
 }
+
+const showCheckpointScreen = (msg) => {
+    checkpointScreen.style.display = "block";
+    checkpointMessage.textContent = msg;
+    if (isCheckpointCollisionDetectionActive) {
+        setTimeout(() => (checkpointScreen.style.display = "none"), 2000);
+    }
+};
 
 startBtn.addEventListener("click", startGame);
 
