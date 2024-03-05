ba70e7c26d70c80540293073af1233b969ba8b98
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-06 00:12:09 +0500

    update stats and radio button ui

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -30,12 +30,25 @@ const rollDice = () => {
     });
 };
 
+const updateStats = () => {
+    currentRoundRollsText.textContent = rolls;
+    currentRoundText.textContent = round;
+};
+
+const updateRadioOption = (optionNode, score) => {
+    scoreInputs[optionNode].disabled = false;
+    scoreInputs[optionNode].value = score;
+    scoreSpans[optionNode].textContent = `, score = ${score}`;
+};
+
 rollDiceBtn.addEventListener("click", () => {
     if (rolls === 3) {
         alert("You have made three rolls this round. Please select a score.");
     } else {
         rolls++;
         rollDice();
+        updateStats();
+        updateRadioOption(0, 10);
     }
 });
 
