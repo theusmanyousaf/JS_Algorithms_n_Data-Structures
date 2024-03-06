3e5bc4745e1173d194cd088944efb440ce70650e
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-06 23:30:14 +0500

    straights in a dice roll algorithm

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -102,6 +102,25 @@ const detectFullHouse = (arr) => {
     updateRadioOption(5, 0);
 };
 
+const checkForStraights = (arr) => {
+    const sortedNumbersArr = arr.sort((a, b) => a - b);
+    const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+    const uniqueNumbersStr = uniqueNumbersArr.join("");
+
+    const smallStraightsArr = ["1234", "2345", "3456"];
+    const largeStraightsArr = ["12345", "23456"];
+
+    if (smallStraightsArr.includes(uniqueNumbersStr)) {
+        updateRadioOption(3, 30);
+    }
+
+    if (largeStraightsArr.includes(uniqueNumbersStr)) {
+        updateRadioOption(4, 40);
+    }
+
+    updateRadioOption(5, 0);
+};
+
 const resetRadioOption = () => {
     scoreInputs.forEach((input) => {
         input.disabled = true;
@@ -143,6 +162,7 @@ rollDiceBtn.addEventListener("click", () => {
         updateStats();
         getHighestDuplicates(diceValuesArr);
         detectFullHouse(diceValuesArr);
+        checkForStraights(diceValuesArr);
     }
 });
 
