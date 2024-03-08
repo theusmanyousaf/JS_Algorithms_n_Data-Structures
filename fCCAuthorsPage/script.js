c5c1274dd58a535077fe367db641039156f6c046
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-08 22:14:19 +0500

    fetch function

--- fCCAuthorsPage/script.js
+++ fCCAuthorsPage/script.js
@@ -4,3 +4,13 @@ const loadMoreBtn = document.getElementById('load-more-btn');
 let startingIndex = 0;
 let endingIndex = 8;
 let authorDataArr = [];
+
+
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+    .then((res) => res.json()) // obtian res data in javascript object notation
+    .then((data) => { // data from json in the form of an array of objects
+        console.log(data);
+    })
+    .catch((err) => { // to address potential errors
+        console.error(`There was an error: ${err}`);
+    });
