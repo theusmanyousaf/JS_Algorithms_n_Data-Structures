5e552ad90b5c0f2fda35001db6ac95a3a08f6f95
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-10 21:06:46 +0500

    data fetching with async await func

--- fCCForumLeaderboard/script.js
+++ fCCForumLeaderboard/script.js
@@ -4,3 +4,14 @@ const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
+
+const fetchData = async () => {
+    try {
+        const res = await fetch(forumLatest);
+        const data = await res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+fetchData();
