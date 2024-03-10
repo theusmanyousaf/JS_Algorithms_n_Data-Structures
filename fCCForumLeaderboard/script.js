02bca677ab0eec5d8c993b7e9ddc2e037a2105f9
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-10 21:05:06 +0500

    initial elements

---
+++ fCCForumLeaderboard/script.js
@@ -0,0 +1,6 @@
+const forumLatest = "https://forum-proxy.freecodecamp.rocks/latest";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer = document.getElementById("posts-container");
