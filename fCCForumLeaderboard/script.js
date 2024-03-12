70eba8e3ad717d2743417a06cf18ee7690503c3f
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-12 21:55:34 +0500

    avatars

--- fCCForumLeaderboard/script.js
+++ fCCForumLeaderboard/script.js
@@ -70,6 +70,21 @@ const viewCount = (views) => {
   return views;
 };
 
+const avatars = (posters, users) => {
+  return posters
+    .map((poster) => {
+      const user = users.find((user) => user.id === poster.user_id);
+      if (user) {
+        const avatar = user.avatar_template.replace(/{size}/, 30);
+        const userAvatarUrl = avatar.startsWith("/user_avatar/")
+          ? avatarUrl.concat(avatar)
+          : avatar;
+        return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
+      }
+    })
+    .join("");
+};
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
@@ -104,7 +119,11 @@ const showLatestPosts = (data) => {
           <p class="post-title">${title}</p>
           ${forumCategory(category_id)}
         </td>
-        <td></td>
+        <td>
+          <div class="avatar-container">
+            ${avatars(posters, users)}
+          </div>
+        </td>
         <td>${posts_count - 1}</td>
         <td>${viewCount(views)}</td>
         <td>${timeAgo(bumped_at)}</td>
