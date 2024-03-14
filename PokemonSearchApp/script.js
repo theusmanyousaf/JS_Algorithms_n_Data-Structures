8ebbbc5072728a58fe1019c41e126fd7abd895d7
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-14 22:01:47 +0500

    pokemon stats and info

--- PokemonSearchApp/script.js
+++ PokemonSearchApp/script.js
@@ -21,6 +21,27 @@ const getPokemon = async () => {
         );
         const data = await response.json();
 
+        // Set Pokémon info
+        pokemonName.textContent = `${data.name.toUpperCase()}`;
+        pokemonID.textContent = `#${data.id}`;
+        weight.textContent = `Weight: ${data.weight}`;
+        height.textContent = `Height: ${data.height}`;
+        spriteContainer.innerHTML = `
+      <img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">
+    `;
+
+        // Set stats
+        hp.textContent = data.stats[0].base_stat;
+        attack.textContent = data.stats[1].base_stat;
+        defense.textContent = data.stats[2].base_stat;
+        specialAttack.textContent = data.stats[3].base_stat;
+        specialDefense.textContent = data.stats[4].base_stat;
+        speed.textContent = data.stats[5].base_stat;
+
+        // Set types
+        types.innerHTML = data.types
+            .map(obj => `<span class="type ${obj.type.name}">${obj.type.name}</span>`)
+            .join('');
     } catch (err) {
         alert('Pokémon not found');
         console.log(`Pokémon not found: ${err}`);
