d56a8fe97c88c056cb261c78cbc1ad404b14f464
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-03-14 22:00:41 +0500

    get pokemon func

--- PokemonSearchApp/script.js
+++ PokemonSearchApp/script.js
@@ -12,3 +12,17 @@ const specialDefense = document.getElementById('special-defense');
 const speed = document.getElementById('speed');
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
+
+const getPokemon = async () => {
+    try {
+        const pokemonNameOrId = searchInput.value.toLowerCase();
+        const response = await fetch(
+            `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`
+        );
+        const data = await response.json();
+
+    } catch (err) {
+        alert('Pokémon not found');
+        console.log(`Pokémon not found: ${err}`);
+    }
+};
